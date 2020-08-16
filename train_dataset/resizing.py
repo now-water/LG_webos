ae674f14a49c99cd37d6ac790de46d1877e7c99e
Jaehyun Bae <54826050+hyunobae@users.noreply.github.com>
2020-08-16 17:44:26 +0900

    Update resizing.py

--- train_dataset/resizing.py
+++ train_dataset/resizing.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
 def resize_multiple_images(src_path, dst_path):
-    # Here src_path is the location where images are saved.
+    
     for filename in os.listdir(src_path):
         try:
             img = Image.open(src_path+filename)
-            new_img = img.resize((120, 120))
+            new_img = img.resize((120, 65))
             if not os.path.exists(dst_path):
                 os.makedirs(dst_path)
             new_img.save(dst_path+filename)
