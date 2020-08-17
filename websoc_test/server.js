cc8c2f30f263b9f4eafe14c622d3a9e6a6da5751
nowwater <nowwater0225@gmail.com>
2020-08-17 21:12:02 +0900

    Updated Websocket Communication

--- websoc_test/server.js
+++ websoc_test/server.js
@@ -9,19 +9,15 @@ var port = options.args[1];
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-app.get('/', function (req, res){
-   res.render('index');
-});
-app.get('/transmit', function(req, res){
+app.get('/', function(req, res){
     // var item = prompt("찾고 싶은 물건 입력: ");
     // options.args.append(item);
     PythonShell.run("websoc_server.py", options, function (err, data) {
         if (err) throw err;
         console.log("받은 데이터 : " + data);
     });
+    res.render('index');
 })
-
 app.listen(port, function () {
     console.log('Connected ' + port)
 });
--- websoc_test/websoc_server.py
+++ websoc_test/websoc_server.py
@@ -1,20 +1,18 @@
 import asyncio
-# 웹 소켓 모듈을 선언한다.	
+# 웹 소켓 모듈을 선언한다.   
 import websockets
 import sys
-
-host = sys.argv[1]
-port = str(int(sys.argv[2]) + 1)
-# 클라이언트 접속이 되면 호출된다.	
+host = "localhost"
+port = "3001"
+# 클라이언트 접속이 되면 호출된다.   
 async def accept(websocket, path):
-  while True:	
+  while True:  
     # 클라이언트로부터 메시지를 대기한다.
     data = await websocket.recv()
     await websocket.send(data + " 가 바라보는 방향에 위치합니다.")
     print(data)
- 	
 # 웹 소켓 서버 생성.호스트는 localhost에 port + 1로 생성한다. (port는 서버 포트여서 다르게 줬다)
 start_server = websockets.serve(accept, host, port);
-# 비동기로 서버를 대기한다.	
+# 비동기로 서버를 대기한다.   
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
