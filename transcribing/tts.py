ee1e2dc62fb6223467c00aba38b23c694b39c76d
Mook <mook@Mookui-MacBookPro.local>
2020-08-23 15:48:05 +0900

    modified tts.py

--- transcribing/tts.py
+++ transcribing/tts.py
@@ -1,4 +1,7 @@
 import requests
+import pyglet
+import time
+import pygame
 
 url = "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
 header = {
@@ -7,12 +10,30 @@ header = {
 }
 data = "<speak> 지옥에서 다시 돌아온 우분투 </speak>"
 
+
+
+def play_and_stop(sound):
+    from threading import Timer
+    player = sound.play()
+    t = Timer(player.source.duration, player.next_source)
+    t.start()
+
+
 response = requests.post(url, headers=header, data=data.encode('utf-8'))
 rescode = response.status_code
 print("Server response code : " + str(rescode))
 if rescode == 200:
-    with open("./result.mp3", 'wb') as f:
+    with open("./result.wav", 'wb') as f:
         f.write(response.content)
         print("Saved audio file!")
+       # sound = pyglet.media.load("./result.mp3",streaming=False)
+       # play_and_stop(sound)
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load("result.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+         time.sleep(0.1)       
 else:
     print("Not valid request!")
+
